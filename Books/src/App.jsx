0b579b6b6447fcc9bdb2af5a7575577def72ef1b
Murat Cengiz <devmuratcengiz@outlook.com>
2025-02-06 19:45:00 +0300

    axios, child to parent communication

---
+++ Books/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [books, setBooks] = useState([]);
+
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
